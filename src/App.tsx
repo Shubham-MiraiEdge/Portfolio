3d9834d2db9669054476c6c60eb5c0b9b207f3e6
shubhampatel9977 <shubhampatel99260@gmail.com>
2025-06-10 17:07:23 +0530

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        <h1>Project working fine With Tailwind CSS</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+
+
+const LandingPage: React.FC = () => {
+    return (
+        <>
+            <div>
+                <Navbar />
+                {/* <Hero /> */}
+            </div>
+        </>
+    )
+};
+
+export default LandingPage;
